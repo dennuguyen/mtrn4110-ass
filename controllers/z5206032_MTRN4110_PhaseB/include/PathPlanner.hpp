c1b9bd904d2cf9f6eee4eba0c5e3ae90da078e59
dennuguyen <dennuguyen@gmail.com>
2021-06-25 14:26:53 +1000

    Initial commit

--- controllers/z5206032_MTRN4110_PhaseB/include/PathPlanner.hpp
+++ controllers/z5206032_MTRN4110_PhaseB/include/PathPlanner.hpp
@@ -1,18 +1,33 @@
 #ifndef PATH_PLANNER_HPP_
 #define PATH_PLANNER_HPP_
 
+#include <map>
 #include <string>
+#include <vector>
 
 namespace mtrn4110 {
 
+using Coordinate = std::pair<int, int>;
+
 class PathPlanner {
    public:
-    explicit PathPlanner(std::string);
+    explicit PathPlanner(std::string const &);
     explicit PathPlanner(PathPlanner const &) = delete;
     PathPlanner(PathPlanner &&) noexcept;
     ~PathPlanner() = default;
 
    private:
+    auto readMapFile(std::string const &) -> void;
+    auto buildGraph() -> void;
+    auto buildDirectedGraph() -> void;
+    auto searchPaths() -> void;
+    auto writePathPlan() const -> void;
+
+   private:
+    std::map<Coordinate, std::pair<int, std::vector<Coordinate>>> directedGraph_;
+    std::vector<std::vector<Coordinate>> paths_;
+    Coordinate start_;
+    Coordinate destination_;
 };
 }  // namespace mtrn4110
 
--- controllers/z5206032_MTRN4110_PhaseB/src/PathPlanner.cpp
+++ controllers/z5206032_MTRN4110_PhaseB/src/PathPlanner.cpp
@@ -2,8 +2,28 @@
 
 namespace mtrn4110 {
 
-PathPlanner::PathPlanner(std::string fileName) {}
+PathPlanner::PathPlanner(std::string const& fileName) {
+    readMapFile(fileName);
+    buildGraph();
+    buildDirectedGraph();
+    searchPaths();
+    writePathPlan();
+}
 
-PathPlanner::PathPlanner(PathPlanner &&pathPlanner) noexcept {}
+PathPlanner::PathPlanner(PathPlanner&& pathPlanner) noexcept
+    : directedGraph_(pathPlanner.directedGraph_),
+      paths_(pathPlanner.paths_),
+      start_(pathPlanner.start_),
+      destination_(pathPlanner.destination_) {}
+
+auto PathPlanner::readMapFile(std::string const& fileName) -> void {}
+
+auto PathPlanner::buildGraph() -> void {}
+
+auto PathPlanner::buildDirectedGraph() -> void {}
+
+auto PathPlanner::searchPaths() -> void {}
+
+auto PathPlanner::writePathPlan() const -> void {}
 
 }  // namespace mtrn4110
