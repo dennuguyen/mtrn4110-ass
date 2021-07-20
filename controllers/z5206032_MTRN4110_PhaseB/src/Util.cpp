67ad70ec2012a9ea39b199f6af32b4c947ade1e0
dan <dennuguyen@gmail.com>
2021-07-20 16:13:33 +1000

    Rename for phase C

--- controllers/z5206032_MTRN4110_PhaseB/src/Util.cpp
+++ controllers/z5206032_MTRN4110_PhaseB/src/Util.cpp
@@ -5,7 +5,7 @@
 namespace mtrn4110 {
 
 auto print(std::string const& t) -> void {
-    std::cout << "[z5206032_MTRN4110_PhaseB] " << t << std::endl;
+    std::cout << "[z5206032_MTRN4110_PhaseC] " << t << std::endl;
 }
 
 }  // namespace mtrn4110
